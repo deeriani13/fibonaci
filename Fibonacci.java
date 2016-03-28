07ac9df0577cec926328357bc5982adefdad5f5c
Ade Iriani Sapitri <deeriani13@gmail.com>
2016-03-28 15:00:27 +0700

    1456

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,24 +6,23 @@ public class Fibonacci {
     static int angka;
 
     public static void main(String[] args) {
-      s = new Scanner(System.in);
+     s = new Scanner(System.in);
         System.out.println("Masukan Jumah Angka: ");
-        angka= s.nextInt();
-        
-     int n1=1,n2=1,n3,i,count=angka;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
-    
- for(i=2;i<angka;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
- }
- 
-        System.out.println("");
-}
+        angka = s.nextInt();
+
+        int n1 = 1, n2 = 1, n3, i, count = angka;
+        System.out.print(n1 + " " + n2);//printing 0 and 1    
+
+        for (i = 2; i < angka; ++i)//loop starts from 2 because 0 and 1 are already printed    
+        {
+            n3 = n1 + n2;
+            System.out.print(" " + n3 );
+            n1 = n2;
+            n2 = n3;
+            sum = sum + n3; 
+        }
+        sum = sum+2;
+        System.out.println("\nJumlah : " + sum);
+    }
 }
 
-11233
-3455
